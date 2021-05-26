c27392fb3dcb3503590369b7f65e8b67b7335023
Abhay Gupta <abhaygupta15153986@gmail.com>
2021-05-26 14:26:48 +0530

    global complete

--- utility.js
+++ utility.js
@@ -2,19 +2,10 @@ let utility = {};
 utility.types = {
     media: ['mp4', 'mkv'],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', 'xz'],
-    documents: [
-        'docx',
-        'doc',
-        'pdf',
-        'xlsx',
-        'odt',
-        'ods',
-        'odg',
-        'odf',
-        'txt',
-        'ps',
-        'tex',
-    ],
+    documents: ['docx', 'doc', 'odt', 'ods', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    pdfs: ['pdf'],
+    excel: ['xlsx', 'xlsm', 'xlsb'],
+    presentation: ['pptx'],
     images: ['jpg', 'png', 'jpeg', 'gif'],
     app: ['exe', 'dmg', 'pkg', 'deb'],
 };
